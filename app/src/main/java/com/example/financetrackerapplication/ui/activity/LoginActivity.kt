a3002f6db32624616ada363ec25f2d99221e6ed4
diwasbk <diwasbk903@gmail.com>
2025-01-29 12:36:29 +0545

    IntroActivity with Animation

--- app/src/main/java/com/example/financetrackerapplication/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/financetrackerapplication/ui/activity/LoginActivity.kt
@@ -1,26 +1,17 @@
 package com.example.financetrackerapplication.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.financetrackerapplication.R
 
-class IntroActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
-        // Delays transition to the next activity by 2 seconds
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@IntroActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        },2000)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
