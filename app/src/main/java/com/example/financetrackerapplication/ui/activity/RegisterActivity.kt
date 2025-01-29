c224200b9d537b66237bca3264f07e6fab64f63f
diwasbk <diwasbk903@gmail.com>
2025-01-29 13:14:13 +0545

    Register Feature

--- app/src/main/java/com/example/financetrackerapplication/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/financetrackerapplication/ui/activity/RegisterActivity.kt
@@ -1,17 +1,31 @@
 package com.example.financetrackerapplication.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.financetrackerapplication.R
+import com.example.financetrackerapplication.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Navigate to Login Activity
+        binding.navigateLogin.setOnClickListener {
+            val explicitIntent = Intent(this, LoginActivity::class.java)
+            startActivity(explicitIntent)
+            finish()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
