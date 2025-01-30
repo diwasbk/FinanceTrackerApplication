ed740f78b5212332d4ff8025045126e6efbd42bc
diwasbk <diwasbk903@gmail.com>
2025-01-30 20:39:31 +0545

    Dashboard with Navigation Features

---
+++ app/src/main/java/com/example/financetrackerapplication/ui/activity/DashboardActivity.kt
@@ -0,0 +1,57 @@
+package com.example.financetrackerapplication.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.financetrackerapplication.R
+import com.example.financetrackerapplication.databinding.ActivityDashboardBinding
+import com.example.financetrackerapplication.ui.fragment.AccountFragment
+import com.example.financetrackerapplication.ui.fragment.AddFragment
+import com.example.financetrackerapplication.ui.fragment.StatementFragment
+import com.example.financetrackerapplication.ui.fragment.StatisticFragment
+
+class DashboardActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Ensure no background highlight
+        binding.bottomNavigationView.itemActiveIndicatorColor = null
+        
+        // To show Statement Fragment by default
+        replaceFragment(StatementFragment())
+        binding.bottomNavigationView.setOnItemSelectedListener {menu->
+            when(menu.itemId){
+                R.id.navStatement -> replaceFragment(StatementFragment())
+                R.id.navAdd -> replaceFragment(AddFragment())
+                R.id.navStatistic -> replaceFragment(StatisticFragment())
+                R.id.navAccount -> replaceFragment(AccountFragment())
+                else -> {}
+            }
+            true
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    // Function to implement Fragments
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameNavigation,fragment)
+        fragmentTransaction.commit()
+    }
+}
